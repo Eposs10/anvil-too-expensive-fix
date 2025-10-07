19afe6759d9adcbd5086ad9be90a6eb52c644ac5
Eposs10 <lunalaluna592@gmail.com>
2025-10-07 22:01:29 +0200

    Support 1.21.X and add client-side fix

---
+++ src/client/java/dev/eposs/atef/AnvilTooExpensiveFixClient.java
@@ -0,0 +1,9 @@
+package dev.eposs.atef;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AnvilTooExpensiveFixClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/client/java/dev/eposs/atef/mixin/client/AnvilScreenMixin.java
@@ -0,0 +1,17 @@
+package dev.eposs.atef.mixin.client;
+
+import net.minecraft.client.gui.screen.ingame.AnvilScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(AnvilScreen.class)
+public abstract class AnvilScreenMixin {
+    public AnvilScreenMixin() {
+    }
+
+    @ModifyConstant(method = "drawForeground", constant = @Constant(intValue = 40))
+    private int mixinTooExpensive(int constant) {
+        return Integer.MAX_VALUE;
+    }
+}
