69663ad2259ec040c9894ad5b0203d6a07dbc572
Eposs <123678741+Eposs10@users.noreply.github.com>
2026-03-28 19:52:24 +0100

    Mc 26.1 (#1)

--- src/main/java/dev/eposs/atef/AnvilTooExpensiveFix.java
+++ src/main/java/dev/eposs/atef/AnvilTooExpensiveFix.java
@@ -1,7 +1,6 @@
 package dev.eposs.atef;
 
 import net.fabricmc.api.ModInitializer;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,6 +17,5 @@ public class AnvilTooExpensiveFix implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
 	}
 }
--- src/main/java/dev/eposs/atef/mixin/AnvilScreenHandlerMixin.java
+++ src/main/java/dev/eposs/atef/mixin/AnvilScreenHandlerMixin.java
@@ -1,19 +1,19 @@
 package dev.eposs.atef.mixin;
 
-import net.minecraft.screen.AnvilScreenHandler;
+import net.minecraft.world.inventory.AnvilMenu;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-@Mixin(AnvilScreenHandler.class)
+@Mixin(AnvilMenu.class)
 public class AnvilScreenHandlerMixin {
 
-    @ModifyConstant(method = "updateResult", constant = @Constant(intValue = 40))
+    @ModifyConstant(method = "createResult", constant = @Constant(intValue = 40))
     private int mixinLimitInt(int i) {
         return Integer.MAX_VALUE;
     }
 
-    @ModifyConstant(method = "updateResult", constant = @Constant(intValue = 39))
+    @ModifyConstant(method = "createResult", constant = @Constant(intValue = 39))
     private int mixinMaxInt(int i) {
         return Integer.MAX_VALUE - 1;
     }
